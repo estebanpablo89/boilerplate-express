306528701565e1f73f67200d4e4d8eaa0a54e334
estebanpablo89 <estebanpablo89@gmail.com>
2020-11-11 20:28:06 -0500

    first commit

---
+++ myApp.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(function logger(req, res, next) {
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+  next();
+});
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/json', (req, res) => {
+  if (process.env.MESSAGE_STYLE === 'uppercase')
+    res.json({ "message": "Hello json".toUpperCase() });
+  else
+    res.json({ "message": "Hello json" });
+});
+
+app.get(
+  '/now',
+  (req, res, next)=>{
+    req.time = new Date().toString();
+    next();
+  }, 
+  (req, res)=>{
+    res.send({
+      time: req.time
+    });
+  }
+);
+
+app.get('/:word/echo', (req, res) => {
+  res.json({echo: req.params.word});
+});
+/*
+app.get('/name', (req,res) => {
+  res.json(req.query);
+});
+*/
+
+app.get('/name', (req,res) => {
+  res.json({
+    'name': `${req.query.first} ${req.query.last}`
+  });
+});
+
+app.post('/name', (req,res) => {
+  res.json({
+    'name': req.body.first+' '+req.body.last
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
